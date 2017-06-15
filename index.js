3443b6542cd05715a362142d864013ee61092144
刘少杰 <439655668@qq.com>
2017-06-15 15:27:50 +0800

    画圈圈

--- index.js
+++ index.js
@@ -2,21 +2,157 @@
  * Created by liushaojie on 2017/6/14.
  */
 
+var num = 599,
+    main = e('.main'),
+    canvasW = 300,
+    canvasH = 500,
+    radius = 50
+
 var testOne = function() {
-    var num = 9,
-        main = e('.main')
-    
-    var testOneFB = function() {
-        
-    }    
-    var testOnePX = function() {
-        
+    var dadClass = `game1`,
+        gameNum = 1
+
+    insert(`依次绘制${num}个圆圈`, dadClass, gameNum, [canvasW, canvasH])
+
+    var testOneNT = function() {
+        var canvas = e(`#nt${gameNum}`),
+            drawCircleNT = function(x, y, r) {
+                var ctx = canvas.getContext("2d")
+                ctx.beginPath()
+                ctx.strokeStyle = 'green'
+                ctx.arc(x, y, r, 0, 2 * Math.PI, false)
+                ctx.stroke()
+            }
+        // 重复次数, 调用方法, 结果反馈选择器, draw的参数
+        test(num, drawCircleNT, `.${dadClass} .ans0`, [canvasW, canvasH, radius])
     }
-    
+
+    var testOneFB = function() {
+        var canvas = new fabric.Canvas(`fb${gameNum}`),
+            drawCircleFB = function(x, y, r) {
+                var circ = new fabric.Circle({
+                    left: x, top: y, radius: r, stroke: 'red', fill: 'transparent'
+                })
+                canvas.add(circ)
+            }
+        // 重复次数, 调用方法, 结果反馈选择器, draw的参数
+        test(num, drawCircleFB, `.${dadClass} .ans1`, [canvasW, canvasH, radius])
+    }
+
+    var testOnePX = function() {
+        var renderer = PIXI.autoDetectRenderer(canvasW, canvasH, {antialias: true, transparent: true, resolution: 1})
+        renderer.view.classList.add('grandson')
+        renderer.view.id = `px${gameNum}`
+        es(`.${dadClass} .son`)[2].appendChild(renderer.view)
+        var stage = new PIXI.Container()
+        var circArr = new PIXI.Container()
+        var drawCirclePX = function(x, y, r) {
+            var circ = new PIXI.Graphics();
+            circ.lineStyle(1, 0x3355ff, 1)
+            // circ.beginFill(0x9966FF);
+            circ.drawCircle(0, 0, r)
+            circ.x = x;
+            circ.y = y;
+            // circ.endFill()
+            circArr.addChild(circ)
+            stage.addChild(circArr)
+            renderer.render(stage)
+        }
+        test(num, drawCirclePX, `.${dadClass} .ans2`, [canvasW, canvasH, radius])
+    }
+    // debugger
+    testOneNT()
+    // debugger
     testOneFB()
+    // debugger
     testOnePX()
 
 }
+
+var testTwo = function() {
+    var dadClass = `game2`,
+        gameNum = 2
+
+
+    insert(`同时绘制${num}个圆圈`, dadClass, gameNum, [canvasW, canvasH])
+
+    var testTwoNT = function() {
+        var canvas = e(`#nt${gameNum}`),
+            drawCircleNT = function(x, y, r) {
+                var ctx = canvas.getContext("2d")
+                ctx.beginPath()
+                ctx.strokeStyle = 'green'
+                ctx.arc(x, y, r, 0, 2 * Math.PI, false)
+                ctx.stroke()
+            }
+        // 重复次数, 调用方法, 结果反馈选择器, draw的参数
+        test(num, drawCircleNT, `.${dadClass} .ans0`, [canvasW, canvasH, radius])
+    }
+
+    var testTwoFB = function() {
+        var canvas = new fabric.Canvas(`fb${gameNum}`),
+            sel = `.${dadClass} .ans1`,
+            arr = [canvasW, canvasH, radius],
+            circArr = []
+            drawCircleFB = function(x, y, r) {
+                var circ = new fabric.Circle({
+                    left: x, top: y, radius: r, stroke: 'red', fill: 'transparent'
+                })
+                circArr.push(circ)
+            }
+        // 重复次数, 调用方法, 结果反馈选择器, draw的参数
+        let t0 = new Date()
+        for(let i = 0; i < num; i++) {
+            drawCircleFB(...rand(...arr))
+            // log(fun.prototype.constructor.name)
+        }
+        // circArr.forEach(function(ele) {
+        //     canvas.add(ele)
+        // })
+        var group = new fabric.Group(circArr, {left: 0, top: 0})
+        canvas.add(group)
+        // let t1 = new Date()
+        let ans = ` 用时 ${new Date() - t0} ms`
+        e(sel).innerText = e(sel).innerText.replace('{{result}}', ans)
+
+
+    }
+
+    var testTwoPX = function() {
+        var renderer = PIXI.autoDetectRenderer(canvasW, canvasH, {antialias: true, transparent: true, resolution: 1})
+        renderer.view.classList.add('grandson')
+        renderer.view.id = `px${gameNum}`
+        es(`.${dadClass} .son`)[2].appendChild(renderer.view)
+        var stage = new PIXI.Container()
+        var circArr = new PIXI.Container()
+        var drawCirclePX = function(x, y, r) {
+            var circ = new PIXI.Graphics();
+            circ.lineStyle(1, 0x3355ff, 1)
+            // circ.beginFill(0x9966FF);
+            circ.drawCircle(0, 0, r)
+            circ.x = x;
+            circ.y = y;
+            // circ.endFill()
+            circArr.addChild(circ)
+            stage.addChild(circArr)
+        }
+        let t0 = new Date()
+        test(num, drawCirclePX, `.${dadClass} .ans2`, [canvasW, canvasH, radius])
+        renderer.render(stage)
+        renderer.render(stage)
+        let t1 = new Date()
+        let ans = `PIXI.js 用时 ${t1 - t0} ms`
+        e(`.${dadClass} .ans2`).innerText = ans
+
+    }
+    // debugger
+    testTwoNT()
+    // debugger
+    testTwoFB()
+    // debugger
+    testTwoPX()
+}
+
 
 var begin = function() {
     var type = "WebGL"
@@ -29,5 +165,6 @@ var begin = function() {
 var __main = function() {
     begin()
     testOne()
+    testTwo()
 }
 __main()
--- src/ultis.js
+++ src/ultis.js
@@ -5,7 +5,8 @@ var e = function(sel) {return document.querySelector(sel)}
 var es = function(sel) {return Array.from(document.querySelectorAll(sel))}
 var log = function(...arg) {console.log.apply(console, arg)}
 // 随机整数生产: 数量, 范围的数组
-var rand = function(num, rangeArr) {
+var rand = function(...rangeArr) {
+    var num = rangeArr.length
     let result = []
     for(let i = 0; i < num; i++) {
         let r = Math.floor(Math.random() * rangeArr[i])
@@ -13,13 +14,13 @@ var rand = function(num, rangeArr) {
     }
     return result
 }
-// 重复次数, 调用方法, 结果反馈选择器
-let test = (num, fun, sel) => {
+// 重复次数, 调用方法, 结果反馈选择器, draw的参数
+let test = (num, fun, sel, arr) => {
 
     let t0 = new Date()
     for(let i = 0; i < num; i++) {
-        fun(...rand(4, [150, 50, 100, 80]))
-        // fun(10, 10, 50, 30)
+        fun(...rand(...arr))
+        // log(fun.prototype.constructor.name)
     }
     let t1 = new Date()
 
@@ -27,21 +28,26 @@ let test = (num, fun, sel) => {
     e(sel).innerText = e(sel).innerText.replace('{{result}}', ans)
 }
 
-var template = (name, dadClass, num, diy) => `
+var template = (name, dadClass, num, arr, diy) => {return `
     <span>${name}</span>
     ${diy || '<span></span>'}
     <div class="dad ${dadClass}">
         
         <div class="son">
+            <span class="ans0">Native{{result}}</span>
+            <canvas class="grandson" id='nt${num}' width=${arr[0]} height=${arr[1]} ></canvas>
+        </div>
+        
+        <div class="son">
             <span class="ans1">Fabric.js{{result}}</span>
-            <canvas class="grandson" id='fb${num}'></canvas>
+            <canvas class="grandson" id='fb${num}' width=${arr[0]} height=${arr[1]} ></canvas>
         </div>
         
         <div class="son">
             <span class="ans2">PIXI.js{{result}}</span>
-            <canvas class="grandson" id='px${num}'></canvas>
         </div>
     </div>`
+}
 
 var insert = (...args) => {
     let t = template(...args)
